5c267a1198d55686856d18a4902e5b6303ec5685
luigoalma <luigoalma@hotmail.com>
2024-12-19 23:34:18 +0000

    Fixes and adjustments to code

--- chrim/gfxFile.cpp
+++ chrim/gfxFile.cpp
@@ -1,11 +1,25 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <filesystem>
+#include <fstream>
+#include <format>
+#include <algorithm>
+
 #include "gfxFile.h"
-using namespace std;
+
 //this class opens the given file as-needed for methods.
-GfxFile::GfxFile(std::filesystem::path Path, int bpp = 4) {
+GfxFile::GfxFile(std::filesystem::path Path, int bpp) {
 	PathAbs = Path;
 	BPP = bpp;
 	TileSize = (bpp / 8) * 0x40;
-	TileBuffer = (char*)malloc(sizeof(char) * (TileSize + 1));
+	TileBuffer = new char[TileSize + 1]; // (char*)malloc(sizeof(char) * (TileSize + 1));
+}
+
+GfxFile::~GfxFile() {
+	//free(TileBuffer);
+	delete[] TileBuffer;
+	TileBuffer = nullptr;
 }
 
 std::filesystem::path GfxFile::GetPath() {
@@ -17,9 +31,9 @@ void GfxFile::Trim() {
 	//work thru in multiples of $40 bytes
 	//when one chunk is all 00 00, chop the file to the beginning of that section
 	//it would be smart to check surrounding rows to make sure they're also empty (large tile support check rows #$0040, #$0100, #$0140 bytes ahead )
-	std::fstream gfxFile(PathAbs.string(), ios_base::in | ios_base::binary);
+	std::fstream gfxFile(PathAbs.string(), std::ios_base::in | std::ios_base::binary);
 	if (!gfxFile.is_open()) {
-		cout << PathAbs.string() + " not found." << endl;
+		std::cout << PathAbs.string() + " not found." << std::endl;
 		return;
 	}
 	int tilesCount = countTiles(gfxFile);
@@ -28,19 +42,15 @@ void GfxFile::Trim() {
 		if (emptyTile) {
 			if (LargeTileEmpty(i, gfxFile)) {
 				//trim the file to the beginning of tile [i]
-				filesystem::resize_file(PathAbs, i * TileSize);
-				string formattedCount = std::format("{:x}", i);
-				transform(formattedCount.begin(), formattedCount.end(), formattedCount.begin(), ::toupper);
-				cout << "Trimmed to $" << formattedCount << " tiles.";
+				std::filesystem::resize_file(PathAbs, i * TileSize);
+				std::string formattedCount = std::format("{:x}", i);
+				std::transform(formattedCount.begin(), formattedCount.end(), formattedCount.begin(), ::toupper);
+				std::cout << "Trimmed to $" << formattedCount << " tiles.";
 				break;
 			}
 		}
 	}
 	gfxFile.close();
-}
-
-void GfxFile::Dispose() {
-	free(TileBuffer);
 }
 
 //char is singular bytes & int is at least 16 bits
@@ -51,19 +61,20 @@ void GfxFile::Dispose() {
 //fstream.read seems to advance the seekg pointer
 //Also important that this char buffer does not need a terminator because it's not being treated as a string.
 
-char* GfxFile::GetTile(int tileNum, fstream& gfxFile) {
-	streampos startPoint = tileNum * TileSize;
-	gfxFile.seekg(startPoint, fstream::beg);
-	gfxFile.read(TileBuffer, (streamsize)TileSize);
-	if ((gfxFile.rdstate() & std::ifstream::failbit) != 0) {
-	}
+char* GfxFile::GetTile(int tileNum, std::fstream& gfxFile) {
+	std::streampos startPoint = tileNum * TileSize;
+	gfxFile.seekg(startPoint, std::fstream::beg);
+	gfxFile.read(TileBuffer, (std::streamsize)TileSize);
+	/*if ((gfxFile.rdstate() & std::ifstream::failbit) != 0) {
+	}*/
 	if ((gfxFile.rdstate() & std::ifstream::eofbit) != 0) {
 		gfxFile.clear(); //clear the error flags for next read
 		std::fill_n(TileBuffer, TileSize, 0);
 		return TileBuffer;
 	}
-	if ((gfxFile.rdstate() & std::ifstream::badbit) != 0) {
-	}
+	/*if ((gfxFile.rdstate() & std::ifstream::badbit) != 0) {
+
+	}*/
 	return TileBuffer;
 }
 
@@ -78,7 +89,7 @@ bool GfxFile::TileIsEmpty(char tileBytes[]) {
 	return true;
 }
 
-bool GfxFile::LargeTileEmpty(int tileNum, fstream& gfxFile) {
+bool GfxFile::LargeTileEmpty(int tileNum, std::fstream& gfxFile) {
 	return
 		TileIsEmpty(GetTile(tileNum + 0x00, gfxFile)) &&
 		TileIsEmpty(GetTile(tileNum + 0x01, gfxFile)) &&
@@ -88,9 +99,9 @@ bool GfxFile::LargeTileEmpty(int tileNum, fstream& gfxFile) {
 
 //assumes that seekg is still where it was when the file was opened?? That seems bad. This could be avoided with class constructor.
 //dividing by tilesize ensures the 8x8 ones will never overflow the file.
-int GfxFile::countTiles(fstream& gfxfile) {
-	std::streampos fileSize = gfxfile.tellg();
-	gfxfile.seekg(0, ios::end);
+int GfxFile::countTiles(std::fstream& gfxfile) {
+	auto fileSize = gfxfile.tellg();
+	gfxfile.seekg(0, std::ios::end);
 	fileSize = gfxfile.tellg() - fileSize;
 	return fileSize / TileSize;
 }
